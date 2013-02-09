a60a44311014cfbea33c02aa46385ab42317214c
Drew <drew@scurvynet.com>
2013-02-08 17:31:02 -0800

    added portage from https://gist.github.com/mongrelP/1205893

---
+++ plugins/portage/portage.plugin.zsh
@@ -0,0 +1,20 @@
+if which eix > /dev/null
+then
+        alias esearch="eix "
+        alias eiup="eix-update "
+        alias eisync="eix-sync "
+        alias esync="eix-sync "
+else
+        alias esearch="emerge --search "
+        alias esync="emerge --sync "
+fi
+ 
+if which revdep-rebuild > /dev/null
+then
+        alias eup="emerge -avuND --fetchonly world && emerge  -vuND --keep-going world && revdep-rebuild && emerge -av --depclean"
+else
+        alias eup="emerge  -avuND world"
+fi
+ 
+alias eresume="emerge --ask --verboze --resume "
+alias emerge="emerge --ask --verboze "
